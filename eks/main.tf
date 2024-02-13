c05d7fb1e3140d238e1886fe6ff6c75c942be017
sfunzbob7 <131837735+sfunzbob7@users.noreply.github.com>
2024-02-13 11:36:02 +0900

    Add files via upload

---
+++ eks/main.tf
@@ -0,0 +1,56 @@
+#create EKS Cluster eks_cluster
+
+resource "aws_eks_cluster" "eks_cluster" {
+  name = var.PROJECT_NAME
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks-AmazonEKSVPCResourceController
+  ]
+
+  # The Amazon Resource Name (ARN) of the IAM role that provides permissions for the Kubernetes control plane to make calls to AWS API operations
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  # Desired Kubernetes master version
+  version = "1.27"
+
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
+  vpc_config {
+    endpoint_private_access = true
+    endpoint_public_access  = false
+    subnet_ids = [
+      data.terraform_remote_state.metabank-eks2.outputs.PRI_SUB3_ID,
+      data.terraform_remote_state.metabank-eks2.outputs.PRI_SUB4_ID
+    ]
+    security_group_ids = [data.terraform_remote_state.metabank-sg.outputs.eks-security]
+  }
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name               = "eks-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_cluster_role.name
+}
---
+++ eks/remote.tf
@@ -0,0 +1,26 @@
+data "terraform_remote_state" "metabank-eks" {
+  backend = "s3"
+  config = {
+    bucket = "metabank-terraform-status"
+    key    = "metabank/Terraform/iam/terraform.tfstate"
+    region = "ap-northeast-2"
+  }
+}
+
+data "terraform_remote_state" "metabank-eks2" {
+  backend = "s3"
+  config = {
+    bucket = "metabank-terraform-status"
+    key    = "metabank/Terraform/vpc/terraform.tfstate"
+    region = "ap-northeast-2"
+  }
+}
+
+data "terraform_remote_state" "metabank-sg" {
+  backend = "s3"
+  config = {
+    bucket = "metabank-terraform-status"
+    key    = "metabank/Terraform/sg/terraform.tfstate"
+    region = "ap-northeast-2"
+  }
+}
